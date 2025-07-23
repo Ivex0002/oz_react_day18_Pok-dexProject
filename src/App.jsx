5e68b73e66d7e4f44906d93289eca5fced35c8b7
ivex0002 <garam199803@gmail.com>
2025-07-23 10:44:48 +0900

    🎨 Added debounce search
    Added debounce search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense, useCallback, useEffect } from "react";
 import "./App.scss";
 import { useDispatch } from "react-redux";
 import { fetchPokemonById } from "./RTK/thunk";
@@ -7,6 +7,7 @@ import { fetchPokemonById } from "./RTK/thunk";
 // import Search from "./pages/Search";
 // import Favorite from "./pages/Favorite";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import debounce from "lodash.debounce";
 
 const Main = lazy(() => import("./pages/Main"));
 const Detail = lazy(() => import("./pages/Detail"));
@@ -23,6 +24,24 @@ function App() {
     dispatch(fetchPokemonById(151));
   }, []);
 
+  const handleSearch = useCallback(
+    debounce((value) => {
+      navigate(`/search?pokemon=${value}`);
+    }, 300),
+    []
+  );
+
+  const onChangeSearch = (e) => {
+    const value = e.target.value.trim();
+    handleSearch(value);
+  };
+
+  useEffect(() => {
+    return () => {
+      handleSearch.cancel();
+    };
+  }, []);
+
   return (
     <>
       <h1 className="title">포켓몬 도감</h1>
@@ -30,10 +49,7 @@ function App() {
         <Link to={"/"}>메인</Link>
         <Link to={"/favorite"}>즐겨찾기</Link>
         <div>
-          <input
-            type="text"
-            onChange={(e) => navigate(`/search?pokemon=${e.target.value}`)}
-          />
+          <input type="text" onChange={(e) => onChangeSearch(e)} />
           <span>🔍</span>
         </div>
       </nav>
